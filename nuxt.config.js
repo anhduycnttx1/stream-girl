2e0300e3f77fb5e790da07b34168d346361ea59a
Duy Do <anhduycnttx1@gmail.com>
2022-10-20 23:17:57 +0700

    dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
     '~/plugins/infiniteloading',
   ],
   generate: {
-    dir: 'public'
+    dir: 'dist'
   },
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: {
